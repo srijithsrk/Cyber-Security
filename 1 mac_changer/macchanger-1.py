28a4e800bf5a94b19fb44df992b2926e4eddf0f8
Srijith <30682653+srijithsrk@users.noreply.github.com>
2019-04-17 22:31:51 +0530

    updated

--- 1 mac_changer/macchanger-1.py
+++ 1 mac_changer/macchanger-1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
-import subprocess
+import subprocess // subprocess is a module used for executing system commands
 
-subprocess.call("ifconfig enp2s0 down",shell=True)
-subprocess.call("ifconfig enp2s0 hw ether 00:11:22:33:44:55",shell=True)
-subprocess.call("ifconfig enp2s0 up",shell=True)
+subprocess.call("ifconfig enp2s0 down",shell=True) // disabling interface and call is the function used
+subprocess.call("ifconfig enp2s0 hw ether 00:11:22:33:44:55",shell=True)  // changing mac address
+subprocess.call("ifconfig enp2s0 up",shell=True)   //enabling interface again
